e3b9ff346225aeb0975c39fb81a31e14a5c076bc
keerthanna-v <67889024+keerthanna-v@users.noreply.github.com>
2020-08-07 12:15:12 +0530

    Add files via upload

---
+++ sms_model.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 16:22:04 2020
+
+@author: keerthanna
+"""
+
+# importing packages
+
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from text_processing import puncation,stopwordsremoval,pstem
+import pickle
+import nltk
+# loading the data set
+
+data=pd.read_csv(r"C:\Users\keerthanna\spam.csv",encoding='latin')
+
+data['text']=data['text'].apply(puncation)
+
+data['text']=data['text'].apply(lambda x: x.lower())
+# removal of stopwords
+
+data['text']=data['text'].apply(stopwordsremoval)
+
+#stemming
+
+data['text']=data['text'].apply(pstem)
+
+# splitng the data set
+
+x_train,x_test,y_train,y_test=train_test_split(data['text'], data['type'], test_size=0.2, random_state=3)
+
+tf= TfidfVectorizer()
+
+tfidf_train=tf.fit_transform(x_train)
+
+tfidf_test=tf.transform(x_test)
+
+pac=PassiveAggressiveClassifier()
+
+pac.fit(tfidf_train,y_train)
+
+
+pickle.dump(pac,open("model.pkl",'wb'))
+pac=pickle.load(open('model.pkl','rb'))
+pickle.dump(tfidf_vectorizer,open("tf.pkl",'wb'))
+tf=pickle.load(open('tf.pkl','rb'))
+tf.transform(["he kja s"])
+pac.predict(tfidf_test[0])
